d805433aa040063239b38937ea8312ea0be941dd
yohans14 <yohans14@yahoo.com>
2022-01-13 17:29:15 -0600

    created enemy object with test and all method

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -8,6 +8,7 @@ test("creates a player object", () => {
   expect(player.health).toEqual(expect.any(Number));
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
+
   expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   );
